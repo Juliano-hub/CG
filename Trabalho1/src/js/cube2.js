26acdcafc1670e25f7da90111929a440aeb1dddd
Juliano-hub <juliano.buss@gmail.com>
2023-03-25 07:38:00 -0300

    adicionando cubo para baixo

--- Trabalho1/src/js/cube2.js
+++ Trabalho1/src/js/cube2.js
@@ -252,8 +252,13 @@ async function main(canvasNUM = "#canvas") {
   }
 
   const extents = getGeometriesExtents(obj.geometries);
+  console.log(extents)
+  extents.max[1] = numShop;
+  extents.min[1] = numShop;
   const range = m4.subtractVectors(extents.max, extents.min);
   // amount to move the object so its center is at the origin
+
+  //range[1] = numShop;
 
   const objOffset = m4.scaleVector(
       m4.addVectors(
@@ -319,13 +324,15 @@ async function main(canvasNUM = "#canvas") {
     // compute the world matrix once since all parts
     // are at the same space.
     let u_world = m4.yRotation(time);
-
+    
     u_world = m4.translate(u_world, ...objOffset);
     
+    /*
     if(canvasNUM != "#canvas"){
       u_world[12] = numShop;
     }
-
+    */
+   
     for (const {bufferInfo, vao, material} of parts) {
       // set the attributes for this part.
       gl.bindVertexArray(vao);
